93e4858a4fe0ea2abf87e6cf17d43fcecdca291b
ying-paris <91549601+ying-paris@users.noreply.github.com>
2021-11-18 13:06:13 +0100

    Updated design and weather icons

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  moscow: {
-    temp: -5,
-    humidity: 20,
-  },
-};
-
 let now = new Date();
 let date = now.getDate();
 let hours = now.getHours();
@@ -95,6 +72,12 @@ function displayWeather(response) {
   let description = response.data.weather[0].description;
   let showDescription = document.querySelector(".descriptionNow");
   showDescription.innerHTML = description;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function showPosition(position) {
