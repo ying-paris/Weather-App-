48c227abdbfc2f6c44193919597f978160b91164
ying-paris <91549601+ying-paris@users.noreply.github.com>
2021-11-30 11:05:28 +0100

    Plugged API data into JS

--- src/index.js
+++ src/index.js
@@ -54,30 +54,37 @@ let form = document.querySelector("form");
 form.addEventListener("submit", submitCity);
 
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-
   let forecastHTML = ` <div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="col-2"> 
-            <div class="forecastDate-day">${day}</div>
+            <div class="forecastDate-day">${formatDay(forecastDay.dt)}</div>
                 <img
-                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt=""
                   width="42"
                 />
                 <div class="forecastDateTemperatures">
-                    <span class="forecastDateTemperaturesHigh">18°</span>
-                    <span class="forecastDateTemperaturesLow">12°</span>
+                    <span class="forecastDateTemperaturesHigh">${Math.round(
+                      forecastDay.temp.max
+                    )}°
+                    }</span>
+                    <span class="forecastDateTemperaturesLow">${Math.round(
+                      forecastDay.temp.min
+                    )}°</span>
                 </div>
         </div> 
     
     `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
